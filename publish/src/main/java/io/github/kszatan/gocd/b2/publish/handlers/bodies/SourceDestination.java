2bc70f7f79c654d3cb617992af64543469009bd0
kszatan <kszatan@gmail.com>
2017-12-28 18:55:42 +0100

    defaults for SourceDestination

--- publish/src/main/java/io/github/kszatan/gocd/b2/publish/handlers/bodies/SourceDestination.java
+++ publish/src/main/java/io/github/kszatan/gocd/b2/publish/handlers/bodies/SourceDestination.java
@@ -7,6 +7,13 @@
 package io.github.kszatan.gocd.b2.publish.handlers.bodies;
 
 public class SourceDestination {
-    public String source;
-    public String destination;
+    public String source = "";
+    public String destination = "";
+
+    public SourceDestination() { }
+
+    public SourceDestination(String source, String destination) {
+        this.source = source;
+        this.destination = destination;
+    }
 }
